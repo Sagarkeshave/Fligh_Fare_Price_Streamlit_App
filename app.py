382ed7e6ab254fd0feaf00ec5fb17448388e5a55
Sagarkeshave <96720769+Sagarkeshave@users.noreply.github.com>
2023-04-29 17:07:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,35 +13,8 @@ model = pickle.load(open("rf_model_streamlit.pkl", "rb"))
 
 # Define the prediction function
 
-def predict(Total_stops,
-            Journey_day,
-            Journey_month,
-            Dep_hour,
-            Dep_min,
-            Arrival_hour,
-            Arrival_min,
-            dur_hour,
-            dur_min,
-            Air_India,
-            GoAir,
-            IndiGo,
-            Jet_Airways,
-            Jet_Airways_Business,
-            Multiple_carriers,
-            Multiple_carriers_Premium_economy,
-            SpiceJet,
-            Trujet,
-            Vistara,
-            Vistara_Premium_economy,
-            Source_Chennai,
-            Source_Delhi,
-            Source_Kolkata,
-            Source_Mumbai,
-            Destination_Cochin,
-            Destination_Delhi,
-            Destination_Hyderabad,
-            Destination_Kolkata,
-            Destination_New_Delhi):
+def predict():
+            
             
             
             
@@ -428,6 +401,7 @@ def predict(Total_stops,
             "Destination_New_Delhi"]
             ))
             return prediction
+            output = round(prediction[0], 2)            
                         
 
 
@@ -444,35 +418,7 @@ Destination = st.selectbox("Destination :" , ["Cochin", "Delhi","New_Delhi","Hyd
 
 
 if st.button('Predict Price'):
-            price = model.predict(Total_stops,
-            Journey_day,
-            Journey_month,
-            Dep_hour,
-            Dep_min,
-            Arrival_hour,
-            Arrival_min,
-            dur_hour,
-            dur_min,
-            Air_India,
-            GoAir,
-            IndiGo,
-            Jet_Airways,
-            Jet_Airways_Business,
-            Multiple_carriers,
-            Multiple_carriers_Premium_economy,
-            SpiceJet,
-            Trujet,
-            Vistara,
-            Vistara_Premium_economy,
-            Source_Chennai,
-            Source_Delhi,
-            Source_Kolkata,
-            Source_Mumbai,
-            Destination_Cochin,
-            Destination_Delhi,
-            Destination_Hyderabad,
-            Destination_Kolkata,
-            Destination_New_Delhi)
+            price = model.predict()
             st.success(f'Your flare for the flight is Rs{price[0]:.2f}')
             
     
